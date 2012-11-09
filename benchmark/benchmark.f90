1ae2298e4cbccdf1a4a43665755c885f36de568f
Dan F-M <danfm@nyu.edu>
2012-11-09 18:55:19 -0500

    plotting scalings

--- benchmark/benchmark.f90
+++ benchmark/benchmark.f90
@@ -15,19 +15,22 @@
 
           r(i) = i * dr
           mu = dsqrt(1.d0 - r(i) * r(i))
-          ir(i) = 1.d0 - u1*(1.d0-mu) - u2*(1.d0-mu)*(1.d0-mu)
+          ir(i) = (1.d0 - u1*(1.d0-mu) - u2*(1.d0-mu)*(1.d0-mu)) &
+                  / (1.d0 - u1 / 3.d0 - u2 / 6.d0)
 
         enddo
 
       end subroutine
 
-      subroutine run_quad(u1, u2, p, nr, nb)
+      subroutine run_quad(u1, u2, p, nr, nb, mat, ust, e)
 
         implicit none
 
         ! Benchmark parameters.
         double precision, intent(in) :: u1, u2, p
         integer, intent(in) :: nb, nr
+
+        double precision, intent(out) :: mat, ust, e
 
         ! System parameters.
         double precision, dimension(nb) :: b, muo1, mu0, lam
@@ -37,9 +40,6 @@
 
         ! Timing.
         integer :: c_s, c_e, rate
-
-        ! Error parameters.
-        double precision :: e
 
         call system_clock(count_rate=rate)
 
@@ -55,10 +55,7 @@
           call occultquad(b, u1, u2, p, muo1, mu0, nb)
         enddo
         call system_clock(count=c_e)
-
-        write(*,*) "Mandel & Agol took:", &
-                   real((c_e - c_s)) / rate / nrep * us, &
-                   "us"
+        mat = dble((c_e - c_s)) / rate / nrep * us
 
         ! Build the histogram approximation to the limb-darkening
         ! profile.
@@ -70,10 +67,7 @@
           call hist_limb_darkening(p, nr, r, ir, nb, b, lam)
         enddo
         call system_clock(count=c_e)
-
-        write(*,*) "We took:", &
-                   real((c_e - c_s)) / rate / nrep * us, &
-                   "us"
+        ust = dble((c_e - c_s)) / rate / nrep * us
 
         ! Compute the error.
         e = 0.d0
@@ -82,8 +76,7 @@
             e = e + dsqrt((muo1(i)-lam(i)) * (muo1(i)-lam(i)))/muo1(i)
           endif
         enddo
-
-        write(*,*) "The relative error is:", e
+        e = e / nb
 
       end subroutine
 
@@ -91,6 +84,14 @@
 
         implicit none
 
-        call run_quad(0.5d0, 0.1d0, 0.1d0, 200, 101)
+        integer :: nr, nb
+        double precision :: mat, ust, e
+
+        do nb=11,101,20
+          do nr=11,201,20
+            call run_quad(0.5d0, 0.1d0, 0.1d0, nr, nb, mat, ust, e)
+            write(*,*) nb, nr, mat, ust, e
+          enddo
+        enddo
 
       end
