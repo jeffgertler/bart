a6277c3eba4957e1f2f22018fe4e3dee81c4bafe
Dan F-M <danfm@nyu.edu>
2012-12-17 00:34:55 +0000

    trying to fix up kepler 4b example

--- examples/kepler4b/kepler4b.py
+++ examples/kepler4b/kepler4b.py
@@ -1,8 +1,9 @@
-import os
-import sys
 import pyfits
 import numpy as np
+import matplotlib.pyplot as pl
 
+import os
+import sys
 dirname = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.dirname(os.path.dirname(dirname)))
 import bart
@@ -11,21 +12,36 @@ import bart
 f = pyfits.open(os.path.join(dirname, "data.fits"))
 lc = np.array(f[1].data)
 f.close()
+
 time = lc["TIME"]
+flux_raw = lc["SAP_FLUX"]
 flux, ferr = lc["PDCSAP_FLUX"], lc["PDCSAP_FLUX_ERR"]
+
+t0 = int(np.median(time[~np.isnan(time)]))
+time = time - t0
+
+# Plot the raw data.
+ax = pl.axes([0.15, 0.15, 0.8, 0.8])
+ax.plot(time - np.min(time[~np.isnan(time)]),
+        1000 * (flux_raw / np.median(flux_raw[~np.isnan(flux_raw)]) - 1),
+        ".k", alpha=0.5)
+ax.set_xlabel("Time [days]")
+ax.set_ylabel(r"Relative Brightness Variation [$\times 10^{-3}$]")
+pl.savefig("kepler4b_raw.png", dpi=300)
 
 # The limb-darkening parameters.
 nbins, gamma1, gamma2 = 100, 0.39, 0.1
 ldp = bart.QuadraticLimbDarkening(nbins, gamma1, gamma2)
 
 if True:
+    # Fit for the LDP with better spacing.
     ldp.bins = np.sqrt(np.linspace(0.0, 1.0, 15)[1:])
     rbins, ir = ldp.bins, ldp.intensity
     ir *= 1.0 / ir[0]
     ldp = bart.LimbDarkening(rbins, ir)
 
 # Initialize the planetary system.
-fs = np.median(flux)
+fs = 1.0
 iobs = 0.0
 system = bart.BART(fs, iobs, ldp)
 
@@ -35,15 +51,16 @@ a = 6.47
 e = 0.0
 T = 3.21346
 phi = 2.90765788
+pomega = 0.0
 i = 89.76
 
 # Add the planet.
-system.add_planet(r, a, e, T, phi, i)
+system.add_planet(r, a, e, T, phi, pomega, i)
 
 # Fit it.
-# print(system.optimize(time, flux, ferr, pars=[u"phi"]))
+print(system.optimize(time, flux, ferr, pars=[u"phi"]))
 system.fit(time, flux, ferr, pars=[u"fs", u"phi", u"T", u"a", u"r",
                                    u"i", u"e", u"ldp"],
-                        niter=2000, thin=10)
+                        niter=200, thin=1, ntrim=0, nwalkers=64)
 system.plot_fit()
 system.plot_triangle()
