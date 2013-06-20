9ae295adf45214147036913341962b1b977eb567
Dan F-M <danfm@nyu.edu>
2013-06-20 18:23:31 +0000

    fitting multiple quarters with GP

--- examples/gp/gp.py
+++ examples/gp/gp.py
@@ -7,8 +7,9 @@ from __future__ import (division, print_function, absolute_import,
 import os
 import sys
 import emcee
-import numpy as np
+import triangle
 import matplotlib.pyplot as pl
+import numpy as np
 import kplr
 from kplr.ld import get_quad_coeffs
 
@@ -34,7 +35,7 @@ ldp = bart.ld.QuadraticLimbDarkening(mu1, mu2).histogram(bins)
 star = bart.Star(ldp=ldp)
 
 # Set up the planet.
-period = 50.0
+period = 300.0
 size = 0.05
 epoch = 10.0
 a = star.get_semimajor(period)
@@ -48,52 +49,72 @@ ps.add_planet(planet)
 
 # Load the data and inject into each transit.
 lcs = kic.get_light_curves(short_cadence=False, fetch=False)
-lc = lcs[6]
 
-with lc.open() as f:
-    # The light curve data are in the first FITS HDU.
-    hdu_data = f[1].data
+# Loop over the datasets and read in the data.
+time, flux, ferr, quality = [np.array([]) for i in range(4)]
+for lc in lcs:
+    with lc.open() as f:
+        # The light curve data are in the first FITS HDU.
+        hdu_data = f[1].data
+        time_, flux_, ferr_, quality = [hdu_data[k] for k in ["time",
+                                                              "sap_flux",
+                                                              "sap_flux_err",
+                                                              "sap_quality"]]
 
-    # Load the data columns.
-    time = hdu_data["time"]
-    time -= np.min(time[np.isfinite(time)])
-    sap_flux = hdu_data["sap_flux"]
-    sap_ferr = hdu_data["sap_flux_err"]
-    quality = hdu_data["sap_quality"]
+    # Mask the missing data.
+    mask = (np.isfinite(time_) * np.isfinite(flux_) * np.isfinite(ferr_)
+            * (quality == 0))
+    time_, flux_, ferr_ = [v[mask] for v in [time_, flux_, ferr_]]
 
-    # Cut out a small chunk of data.
-    inds = (time < 30.0) * np.isfinite(time) * np.isfinite(sap_flux)
-    time = time[inds]
-    sap_flux = sap_flux[inds]
-    sap_ferr = sap_ferr[inds]
+    # Inject the transit.
+    flux_ *= ps.lightcurve(time_)
 
-# Inject a transit.
-sap_flux *= ps.lightcurve(time)
+    # Normalize the data.
+    mu = np.median(flux_)
 
-# Normalize the data.
-mu = np.median(sap_flux)
-sap_flux /= mu
-sap_ferr /= mu
+    time = np.append(time, time_)
+    flux = np.append(flux, flux_ / mu)
+    ferr = np.append(ferr, ferr_ / mu)
+
+# Cut out only data around transits.
+inds = np.abs((time - epoch + 0.5 * period) % period - 0.5 * period) < 5
+time, flux, ferr = time[inds], flux[inds], ferr[inds]
+
+pl.plot(time % period, flux, ".k")
+pl.savefig("data.png")
 
 
 def lnlike(p):
-    planet.t0, planet.size = p[0], np.exp(p[1])
-    model = sap_flux / ps.lightcurve(time)
-    return _george.lnlikelihood(time, model, sap_ferr, 1.0, 3.0)
+    if not 0 < p[1] < 1 or p[2] <= 1 or not 0 <= p[3] < 1:
+        return -np.inf
+    planet.t0, planet.r, planet.a, b = p
+    ps.iobs = np.degrees(np.arctan2(planet.a, b))
+    model = flux / ps.lightcurve(time) - 1
+    return _george.lnlikelihood(time, model, ferr, 1.0, 3.0)
 
 
 if __name__ == "__main__":
     nwalkers = 10
-    p0 = zip(epoch + 0.01 * np.random.randn(nwalkers),
-             np.log(np.abs(size * (1 + 0.01 * np.random.randn(nwalkers)))))
+    p0 = zip(epoch + 0.001 * np.random.randn(nwalkers),
+             np.abs(size * (1 + 0.01 * np.random.randn(nwalkers))),
+             np.abs(a * (1 + 1e-4 * np.random.randn(nwalkers))),
+             np.abs(b * (1 + 0.01 * np.random.randn(nwalkers))))
 
     sampler = emcee.EnsembleSampler(nwalkers, len(p0[0]), lnlike,
                                     threads=nwalkers)
     fn = "samples.txt"
     with open(fn, "w") as f:
-        f.write("# t0 ln(size) ln(prob)\n")
-    for pos, lnprob, state in sampler.sample(p0, iterations=10,
+        f.write("# t0 r/R a/R b/R ln(prob)\n")
+    for pos, lnprob, state in sampler.sample(p0, iterations=500,
                                              storechain=False):
         with open(fn, "a") as f:
             for p, lp in zip(pos, lnprob):
-                f.write("{0} {1} {2}\n".format(p[0], p[1], lp))
+                f.write("{0} {1}\n".format(
+                    " ".join(map("{0}".format, p)), lp))
+
+    print("Acceptance fraction: {0}"
+          .format(np.mean(sampler.acceptance_fraction)))
+
+    samples = np.loadtxt(fn)
+    figure = triangle.corner(samples)
+    figure.savefig("triangle.png")
