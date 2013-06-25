9e3c8f65a9e1e772411be0c53a618f1457c908a4
Dan F-M <danfm@nyu.edu>
2013-06-25 14:38:50 -0400

    Poisson likelihood for photon dataset

--- bart/data.py
+++ bart/data.py
@@ -56,6 +56,13 @@ class LightCurve(Dataset):
         self.K = K
 
     def lnlike(self, model):
+        """
+        Get the likelihood of this dataset given a particular :class:`Model`.
+
+        :param model:
+            The :class:`Model` specifying the model to compare the data to.
+
+        """
         lc = model.planetary_system.lightcurve(self.time, texp=self.texp,
                                                K=self.K)
         return np.sum(-0.5 * (lc - self.flux) ** 2)
@@ -98,7 +105,86 @@ class GPLightCurve(LightCurve):
         self.l2 = l2
 
     def lnlike(self, model):
+        """
+        Get the likelihood of this dataset given a particular :class:`Model`.
+
+        :param model:
+            The :class:`Model` specifying the model to compare the data to.
+
+        """
         lc = model.planetary_system.lightcurve(self.time, texp=self.texp,
                                                K=self.K)
         return _george.lnlikelihood(self.time, self.flux / lc - 1, self.ferr,
                                     self.alpha, self.l2)
+
+
+class PoissonLightCurve(LightCurve):
+    """
+    An extension to :class:`LightCurve` with a Poisson likelihood function.
+    This class automatically masks all NaNs in the data stream.
+
+    :param time:
+        The times of the samples in days.
+
+    :param dt: (optional)
+        The bin size in days. (default: 0.1)
+
+    """
+
+    def __init__(self, time, dt=0.1, K=3):
+        self.time = time[np.isfinite(time)]
+        self.dt = dt
+
+    def lnlike(self, model):
+        """
+        Get the likelihood of this dataset given a particular :class:`Model`.
+
+        :param model:
+            The :class:`Model` specifying the model to compare the data to.
+
+        """
+        photonrates = self.rate(model, self.time)
+        bintimes = np.arange(self.time.min(), self.time.max(), self.dt)
+        binrates = self.rate(bintimes)
+        prob = np.sum(np.log(photonrates)) - self.dt * np.sum(binrates)
+        return prob
+
+    def rate(self, model, t):
+        """
+        Return the rate estimate for a set of time samples. This function
+        combines the estimated light curve, the sensitivity function and the
+        background rate.
+
+        :param model:
+            The :class:`Model` to compute the estimate for.
+
+        :param t:
+            The time points in days.
+
+        """
+        lc = model.planetary_system.lightcurve(t, texp=0, K=1)
+        return lc * self.sensitivity(t) + self.background(t)
+
+    def background(self, t):
+        """
+        The background function. The default implementation is a trivial zero
+        level background. Subclasses should overload this when they have a
+        better understanding of the instrument.
+
+        :param t:
+            The time points in days.
+
+        """
+        return np.zeros_like(t)
+
+    def sensitivity(self, t):
+        """
+        The sensitivity function of the instrument. The default implementation
+        simply returns an array of ones so subclasses should return something
+        more sophisticated.
+
+        :param t:
+            The time points in days.
+
+        """
+        return np.ones_like(t)
