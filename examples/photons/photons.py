12c168d9703b18de9fcbae15f43f7c7cc0d2ed2b
Dan F-M <danfm@nyu.edu>
2013-06-25 15:13:45 -0400

    starting photons example

---
+++ examples/photons/photons.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+import os
+import sys
+import emcee
+import time as timer
+import triangle
+import matplotlib.pyplot as pl
+import numpy as np
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(
+                os.path.abspath(__file__)))))
+
+import bart
+from bart.parameters import (Parameter, ImpactParameter, PeriodParameter,
+                             LogMultiParameter)
+from bart.priors import UniformPrior, NormalPrior
+
+
+def setup():
+    bins = np.linspace(0, 1, 50)[1:] ** 0.5
+    ldp = bart.ld.QuadraticLimbDarkening(0.3, 0.1).histogram(bins)
+
+    # Set up the planetary system.
+    star = bart.Star(ldp=ldp)
+    planet = bart.Planet(1.1, 400., t0=5.0)
+    ps = bart.PlanetarySystem(star)
+    ps.add_planet(planet)
+
+    # Initialize the model.
+    model = bart.Model(ps)
+
+    # Generate some fake data.
+    dt = 0.001
+    tbins = np.arange(0.0, 10.0, dt)
+    lc = 5000 * ps.lightcurve(tbins, texp=0, K=1)
+    times = np.array([])
+    for t, r in zip(tbins, lc):
+        times = np.append(times, t + dt *
+                          np.random.rand(np.random.poisson(lam=dt * r)))
+
+    n, bins, p = pl.hist(times, 50)
+    pl.plot(tbins, lc)
+    pl.savefig("data.png")
+
+    return model
+
+
+if __name__ == "__main__":
+    model = setup()
