0aaf95a3a7d9553d5ea5137b29c3844f7414ad2b
Dan F-M <danfm@nyu.edu>
2013-07-06 17:25:23 +0000

    slight refactor and generalization of GP -> OO

--- include/george.h
+++ include/george.h
@@ -2,7 +2,47 @@
 #define _GEORGE_H_
 
 #ifdef __cplusplus
+#include <Eigen/Dense>
+
 #define EXTERNC extern "C"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using Eigen::LDLT;
+
+class George
+{
+
+    private:
+
+        int nsamples_;
+        double *x_;
+
+        int npars_;
+        double *pars_;
+        double (*kernel_) (double x1, double x2, int npars, double *pars);
+
+        bool computed_;
+        LDLT<MatrixXd> L_;
+
+    public:
+
+        George (int npars, double *pars,
+                double (*kernel) (double, double, int, double*)) {
+            computed_ = false;
+            npars_ = npars;
+            pars_ = pars;
+            kernel_ = kernel;
+            nsamples_ = 0;
+        };
+
+        int compute (int nsamples, double *x, double *yerr);
+        double lnlikelihood (int nsamples, double *y);
+        int predict (int nsamples, double *y, int ntest, double *x,
+                     double *mu, double *cov);
+
+};
+
 #else
 #define EXTERNC
 #endif
@@ -12,8 +52,8 @@ EXTERNC double gp_lnlikelihood (int nsamples, double *x, double *y,
 EXTERNC int gp_predict (int nsamples, double *x, double *y, double *yerr,
                         double amp, double var, int ntest, double *xtest,
                         double *mean, double *cov);
-EXTERNC double gp_isotropic_kernel (double x1, double x2, double amp,
-                                       double var);
+EXTERNC double gp_isotropic_kernel (double x1, double x2, int npars,
+                                    double *pars);
 
 #undef EXTERNC
 
--- src/george.cpp
+++ src/george.cpp
@@ -1,98 +1,90 @@
-#include <Eigen/Dense>
 #include "george.h"
+
+#define TWOLNPI 1.8378770664093453
 
 using namespace Eigen;
 
-double gp_lnlikelihood (int nsamples, double *x, double *y, double *yerr,
-                        double amp, double var)
-{
-    int i, j;
-    double logdet, value;
-    MatrixXd Kxx(nsamples, nsamples);
-    LDLT<MatrixXd> L;
-    VectorXd alpha, yvec = Map<VectorXd>(y, nsamples);
-
-    // Build the kernel matrix.
-    // NOTE: I'm assuming a symmetric kernel.
-    for (i = 0; i < nsamples; ++i) {
-        for (j = i + 1; j < nsamples; ++j) {
-            value = gp_isotropic_kernel(x[i], x[j], amp, var);
-            Kxx(i, j) = value;
-            Kxx(j, i) = value;
-        }
-        Kxx(i, i) = gp_isotropic_kernel(x[i], x[i], amp, var)
-                    + yerr[i] * yerr[i];
-    }
-
-    // Compute the decomposition of K(X, X)
-    L = LDLT<MatrixXd>(Kxx);
-    if (L.info() != Success)
-        return -INFINITY;
-
-    // Solve the system.
-    alpha = L.solve(yvec);
-    if (L.info() != Success)
-        return -INFINITY;
-
-    // Compute the likelihood value.
-    logdet = log(L.vectorD().array()).sum();
-    return -0.5 * (yvec.transpose() * alpha + logdet
-           + nsamples * log(2 * M_PI));
-}
-
-int gp_predict (int nsamples, double *x, double *y, double *yerr, double amp,
-                double var, int ntest, double *xtest, double *mean,
-                double *cov)
+int George::compute (int nsamples, double *x, double *yerr)
 {
     int i, j;
     double value;
-    MatrixXd Kxx(nsamples, nsamples), Kxs(nsamples, ntest),
-             Kss(ntest, ntest);
     LDLT<MatrixXd> L;
-    VectorXd alpha, ytest, yvec = Map<VectorXd>(y, nsamples);
+    MatrixXd Kxx(nsamples, nsamples);
 
-    // Build the kernel matrices.
-    // NOTE: I'm assuming a symmetric kernel.
+    nsamples_ = nsamples;
+    x_ = x;
+
     for (i = 0; i < nsamples; ++i) {
         for (j = i + 1; j < nsamples; ++j) {
-            value = gp_isotropic_kernel(x[i], x[j], amp, var);
+            value = kernel_(x[i], x[j], npars_, pars_);
             Kxx(i, j) = value;
             Kxx(j, i) = value;
         }
-        Kxx(i, i) = gp_isotropic_kernel(x[i], x[i], amp, var)
-                    + yerr[i] * yerr[i];
-        for (j = 0; j < ntest; ++j)
-            Kxs(i, j) = gp_isotropic_kernel(x[i], xtest[j], amp, var);
+        Kxx(i, i) = kernel_(x[i], x[i], npars_, pars_) + yerr[i] * yerr[i];
     }
 
-    // NOTE: I'm assuming a symmetric kernel here too!
+    L_ = LDLT<MatrixXd>(Kxx);
+    if (L.info() != Success) return -1;
+
+    computed_ = true;
+    return 0;
+}
+
+double George::lnlikelihood (int nsamples, double *y)
+{
+    double logdet;
+    VectorXd det, alpha, yvec = Map<VectorXd>(y, nsamples);
+
+    if (!computed_ || nsamples != nsamples_)
+        return -INFINITY;
+
+    alpha = L_.solve(yvec);
+    if (L_.info() != Success)
+        return -INFINITY;
+
+    det = L_.vectorD().array();
+    if (det.minCoeff() < 0)
+        return -INFINITY;
+
+    logdet = log(det).sum();
+    return -0.5 * (yvec.transpose() * alpha + logdet + nsamples * TWOLNPI);
+}
+
+int George::predict (int nsamples, double *y, int ntest, double *x,
+                     double *mu, double *cov)
+{
+    int i, j;
+    double value;
+    MatrixXd Kxs(nsamples_, ntest), Kss(ntest, ntest);
+    VectorXd alpha, ytest, yvec = Map<VectorXd>(y, nsamples);
+
+    if (!computed_ || nsamples != nsamples_)
+        return -1;
+
+    // Build the kernel matrices.
+    for (i = 0; i < nsamples_; ++i)
+        for (j = 0; j < ntest; ++j)
+            Kxs(i, j) = kernel_(x_[i], x[j], npars_, pars_);
     for (i = 0; i < ntest; ++i) {
-        Kss(i, i) = gp_isotropic_kernel(xtest[i], xtest[i], amp, var);
+        Kss(i, i) = kernel_(x[i], x[i], npars_, pars_);
         for (j = i + 1; j < ntest; ++j) {
-            value = gp_isotropic_kernel(xtest[i], xtest[j], amp, var);
+            value = kernel_(x[i], x[j], npars_, pars_);
             Kss(i, j) = value;
             Kss(j, i) = value;
         }
     }
 
-    // Compute the decomposition of K(X, X)
-    L = LDLT<MatrixXd>(Kxx);
-    if (L.info() != Success)
-        return -1;
-
-    // Solve the system.
-    alpha = L.solve(yvec);
-    if (L.info() != Success)
+    alpha = L_.solve(yvec);
+    if (L_.info() != Success)
         return -2;
 
     // Compute and copy the predictive mean vector.
     ytest = Kxs.transpose() * alpha;
-    for (i = 0; i < ntest; ++i)
-        mean[i] = ytest[i];
+    for (i = 0; i < ntest; ++i) mu[i] = ytest[i];
 
     // Compute the predictive covariance matrix.
-    Kss -= Kxs.transpose() * L.solve(Kxs);
-    if (L.info() != Success)
+    Kss -= Kxs.transpose() * L_.solve(Kxs);
+    if (L_.info() != Success)
         return -3;
     for (i = 0; i < ntest; ++i)
         for (j = 0; j < ntest; ++j)
@@ -101,8 +93,38 @@ int gp_predict (int nsamples, double *x, double *y, double *yerr, double amp,
     return 0;
 }
 
-double gp_isotropic_kernel (double x1, double x2, double amp, double var)
+// C interface functions.
+
+double gp_lnlikelihood (int nsamples, double *x, double *y, double *yerr,
+                        double amp, double var)
 {
-    double d = x1 - x2;
-    return amp * exp(-0.5 * d * d / var);
+    int info;
+    double pars[2] = {amp, var};
+    George gp(2, pars, &gp_isotropic_kernel);
+    info = gp.compute(nsamples, x, yerr);
+    if (info != 0)
+        return -INFINITY;
+    return gp.lnlikelihood(nsamples, y);
+}
+
+int gp_predict (int nsamples, double *x, double *y, double *yerr, double amp,
+                double var, int ntest, double *xtest, double *mean,
+                double *cov)
+{
+    int info;
+    double pars[2] = {amp, var};
+    George gp(2, pars, &gp_isotropic_kernel);
+    info = gp.compute(nsamples, x, yerr);
+    if (info != 0)
+        return info;
+    return gp.predict(nsamples, y, ntest, xtest, mean, cov);
+}
+
+double gp_isotropic_kernel (double x1, double x2, int npars, double *pars)
+{
+    int i;
+    double d = x1 - x2, chi2 = -0.5 * d * d, value = 0.0;
+    for (i = 0; i + 1 < npars; i += 2)
+        value += pars[i] * exp(chi2 / pars[i + 1]);
+    return value;
 }
