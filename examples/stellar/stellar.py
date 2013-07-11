ddea38e96eebf8802b8d24067e85a852b08106de
Dan F-M <danfm@nyu.edu>
2013-07-11 10:11:54 +0000

    optimizing and sampling several chunks

--- examples/stellar/stellar.py
+++ examples/stellar/stellar.py
@@ -17,22 +17,30 @@ import scipy.optimize as op
 # Get some datasets but don't actually
 datasets, ps = kepler_injection(2301306, 400.0, 0.0)
 
+time, flux, ferr = None, None, None
 dt = 10.0
 
-ds = datasets[8]
-tmn, tmx = ds.time.min(), ds.time.max()
-t0 = tmn + (tmx - tmn) * np.random.rand()
-m = (ds.time > t0) * (ds.time < t0 + dt)
 
-time, flux, ferr = ds.time[m], ds.flux[m] - 1, ds.ferr[m]
+def random_dataset():
+    dsn = np.random.randint(len(datasets))
+    ds = datasets[dsn]
+    tmn, tmx = ds.time.min(), ds.time.max()
+    t0 = tmn + (tmx - tmn) * np.random.rand()
+    m = (ds.time > t0) * (ds.time < t0 + dt)
+    return dsn, ds.time[m], ds.flux[m] - 1, ds.ferr[m]
 
-pl.plot(time, flux, ".k")
-pl.savefig("data.png")
+data = [random_dataset()[1:] for i in range(10)]
 
 
 def lnprobfn(p):
     p = np.exp(p)
     return _george.lnlikelihood(time, flux, ferr, p[0], p[1])
+
+
+def full_lnprobfn(p):
+    p = np.exp(p)
+    return np.sum([_george.lnlikelihood(d[0], d[1], d[2], p[0], p[1])
+                   for d in data])
 
 
 def loss(p):
@@ -41,14 +49,55 @@ def loss(p):
         return -ll, -g
     return np.inf, -g
 
-p0 = np.exp([-1.0, 1.5])
-results = op.minimize(loss, p0, jac=True, method="L-BFGS-B",
+
+def full_loss(p):
+    ll, g = zip(*[_george.gradlnlikelihood(d[0], d[1], d[2], p[0], p[1])
+                  for d in data])
+    if np.all(np.isfinite(ll)):
+        return -np.sum(ll), -sum(g)
+    return np.inf, -g[0]
+
+
+# Fit a bunch of datasets simultaneously.
+p0 = [10 ** -5, 1.5]
+results = op.minimize(full_loss, p0, jac=True, method="L-BFGS-B",
                       bounds=[(0, None), (0, None)])
 p = results.x
-print(p)
+print(p, results.success)
+
+# Sample the posterior.
+ndim, nwalkers = 2, 20
+p0 = [np.log(p) + 1e-4 * np.random.randn(2) for i in range(nwalkers)]
+sampler = emcee.EnsembleSampler(nwalkers, ndim, full_lnprobfn, threads=10)
+print("sampling")
+sampler.run_mcmc(p0, 1000)
+
+print("plotting")
+pl.clf()
+figure = triangle.corner(sampler.flatchain, truths=np.log(p))
+figure.savefig("triangle.png")
+
+assert 0
+
+
+fn = "hyperpars.txt"
+for i in range(200):
+    dsn, time, flux, ferr = random_dataset()
+
+    p0 = [10 ** -5, 0.1]
+    results = op.minimize(loss, p0, jac=True, method="L-BFGS-B",
+                          bounds=[(0, None), (0, None)])
+
+    p = results.x
+    print(dsn, p, results.success)
+    if results.success:
+        with open(fn, "a") as f:
+            f.write("{0} {1} {2}\n".format(dsn, p[0], p[1]))
+
+assert 0
 
 # Plot predictions.
-t = np.linspace(t0, t0 + dt, 100)
+t = np.linspace(time.min(), time.max(), 100)
 mu, cov = _george.predict(time, flux, ferr, p[0], p[1], t)
 model = np.random.multivariate_normal(mu, cov, size=50)
 pl.plot(t, model.T, "k", alpha=0.05)
